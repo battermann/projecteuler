fb4eeabe61c9b34dd20ddbb92dcc728fd9c1d1dd
Leif Battermann <leifbattermann@gmail.com>
2014-11-01 09:58:38 +0100

    problem 2

---
+++ projecteuler/projecteuler.tests/problem2tests.fs
@@ -0,0 +1,15 @@
+﻿module projecteuler.problem2tests
+
+open projecteuler.problem2
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``limit of 50 should equal 44`` ()
+    = solve 50 |> should equal 44
+
+[<Test>]
+let ``limit of 4000000 should equal `` ()
+    = solve 4000000 |> should equal 4613732
+
+
