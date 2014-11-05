f4214485ec0a738f9ca88b1218c095b86a65dff7
Leif Battermann <leif.battermann@webrunners.de>
2014-11-05 11:04:06 +0100

    problem 3 solved with clojure

--- projecteuler.clojure/src/projecteuler/problem3.clj
+++ projecteuler.clojure/src/projecteuler/problem3.clj
@@ -1,15 +1,16 @@
 (ns projecteuler.problem3)
 
 ;The prime factors of 13195 are 5, 7, 13 and 29.
-;What is the largest prime factor of the number 600851475143 ?
+;What is the largest prime factor of the candidate 600851475143 ?
 
-; Works with smaller numbers. Apparently not efficient enough.
 (defn solve [n]
-	(loop [number n
-		     current (quot n 2)
-		     largest n]
-		(if (< current 2)
-			largest
-			(if (= (mod number current) 0) 
-				(recur current (quot current 2) current) 
-				(recur number (dec current) largest))))) 
+	(defn is-divisible-by [x y] (if (= (rem x y) 0) true false))
+	(defn fast-inc [i] (if (is-divisible-by i 2) (inc i) (+ i 2)))
+	
+	(loop [candidate n
+		   divisor 2]
+		(if (> divisor (/ candidate 2))
+			candidate
+			(if (is-divisible-by candidate divisor)
+				(recur (/ candidate divisor) 2)
+				(recur candidate (fast-inc divisor))))))
--- projecteuler.clojure/test/projecteuler/problem3_tests.clj
+++ projecteuler.clojure/test/projecteuler/problem3_tests.clj
@@ -2,16 +2,19 @@
   (:require [clojure.test :refer :all]
             [projecteuler.problem3 :refer :all]))
 
+(deftest problem3-time-function
+  (testing "function execution time problem 3"
+    (time (solve 600851475143))))
+
 (deftest problem3-tests
-  	(testing "project euler problem 3"
-  		(is (= (solve 2 ) 2))
-  		(is (= (solve 3 ) 3))
-  		(is (= (solve 4 ) 2))
-  		(is (= (solve 21 ) 7))
-  		(is (= (solve 23 ) 23))
-  		(is (= (solve 13195) 29))))
+	(testing "project euler problem 3"
+  	(is (= (solve 2 ) 2))
+  	(is (= (solve 3 ) 3))
+  	(is (= (solve 4 ) 2))
+  	(is (= (solve 21 ) 7))
+  	(is (= (solve 23 ) 23))
+  	(is (= (solve 13195) 29))))
 
 (deftest problem3-tests2
-    (testing "project euler problem 3"
-      (is (= (solve 600851475143) 29))))
-
+  (testing "project euler problem 3"
+    (is (= (solve 600851475143) 6857))))
