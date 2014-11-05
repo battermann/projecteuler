e387b8a713c1959b4405f7e9be54ce2eba7d85be
Leif Battermann <leif.battermann@webrunners.de>
2014-11-05 12:59:49 +0100

    problem 3 solved in f#

---
+++ projecteuler.fsharp/projecteuler.tests/problem3tests.fs
@@ -0,0 +1,19 @@
+﻿module problem3tests
+
+open problem3
+open NUnit.Framework
+open FsUnit
+
+[<TestCase(2,2)>]
+[<TestCase(3,3)>]
+[<TestCase(4, 2)>]
+[<TestCase(21,7)>]
+[<TestCase(23,23)>]
+[<TestCase(13195,29)>]
+let ``largest prime factor should equal`` ((n:int), (expected:int)) = 
+    solve (n |> int64) |> should equal (expected |> int64)
+
+[<Test>]
+let ``largest prime factor of 600851475143 should equal 6857`` ()
+    = solve 600851475143L |> should equal 6857L
+
---
+++ projecteuler.fsharp/projecteuler/problem3.fs
@@ -0,0 +1,18 @@
+﻿module problem3
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+let solve n =
+    let fastinc i = 
+        match i with
+        | i when i % 2L = 0L -> i + 1L
+        | _ -> i + 2L
+
+    let rec solve candidate divisor =
+        match divisor with
+        | d when d > (candidate / 2L) -> candidate
+        | d when candidate % d = 0L -> solve (candidate / divisor) 2L
+        | _ -> solve candidate (fastinc divisor)
+
+    solve n 2L
