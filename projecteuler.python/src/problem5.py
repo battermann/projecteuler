3e17eb910b821ebb1331de82fdc4d88414818995
Leif Battermann <leif.battermann@webrunners.de>
2014-11-12 13:56:51 +0100

    problem 5 refactored (python)

--- projecteuler.python/src/problem5.py
+++ projecteuler.python/src/problem5.py
@@ -1,16 +1,11 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-def ggt(a, b):
-  rest = a%b
-  while rest != 0:
-    a = b
-    b = rest
-    rest = a%b
-  return b
+def gcd(a, b):
+  return a if b == 0 else gcd(b, a%b)
 
-def kgv(a, b):
-  return abs(a*b)/ggt(a,b)
+def lcm(a, b):
+  return abs(a*b)/gcd(a,b)
 
 def solve(n):
-  return reduce(lambda x,y: kgv(x,y), range(1, n+1))
+  return reduce(lambda x,y: lcm(x,y), range(1, n+1))
--- projecteuler.python/tests/problem5tests.py
+++ projecteuler.python/tests/problem5tests.py
@@ -4,14 +4,14 @@ import problem5
 
 class Problem5Tests(unittest.TestCase):
 
-  def test_kgv(self):
-    self.assertEqual(problem5.kgv(2, 2), 2)
-    self.assertEqual(problem5.kgv(10, 15), 30)
+  def test_lcm(self):
+    self.assertEqual(problem5.lcm(2, 2), 2)
+    self.assertEqual(problem5.lcm(10, 15), 30)
 
-  def test_smallest_evenly_devisible(self):
+  def test_lcm_up_to_10(self):
     self.assertEqual(problem5.solve(10), 2520)
 
-  def test_smallest_evenly_devisible(self):
+  def test_lcm_up_to_20(self):
     self.assertEqual(problem5.solve(20), 232792560)
 
 if __name__ == '__main__':
